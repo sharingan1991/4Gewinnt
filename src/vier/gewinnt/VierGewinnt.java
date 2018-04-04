98341bf2e9ffed6982b11a1063fa328c44d5ee76
Sharingan1991 <senschwarz@yahoo.de>
2018-04-05 01:51:53 +0200

    Initial commit.

--- src/vier/gewinnt/VierGewinnt.java
+++ src/vier/gewinnt/VierGewinnt.java
@@ -5,7 +5,7 @@ package vier.gewinnt;
  */
 public class VierGewinnt{
 
-    private final char[][] spielfeld = new char[7][6];
+    private final char[][] spielfeld = new char[7][6]; //Spalten/Zeilen
     private final Inputlistener listener;
     
     private boolean spieler = true; // Spieler1 = True, Spieler2= False
@@ -20,9 +20,9 @@ public class VierGewinnt{
     
         public char[][] Spielreset(char spielfeld[][]){
         
-            for (char j = 0; j < spielfeld.length; j++) // j = Zeilen
+            for (int j = 0; j < 6; j++) // j = Zeilen
 		{
-			for (char i = 0; i < spielfeld[j].length; i++) // i = Spalten
+			for (int i = 0; i < 7; i++) // i = Spalten
 			{
 				spielfeld[j][i] = ('9');
                         }
