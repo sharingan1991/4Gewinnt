dab37c9025f42b217c846302e4ad39111fa1433d
Sharingan1991 <senschwarz@yahoo.de>
2018-04-04 22:47:32 +0200

    Initial commit.

--- src/vier/gewinnt/VierGewinnt.java
+++ src/vier/gewinnt/VierGewinnt.java
@@ -3,43 +3,32 @@ package vier.gewinnt;
  *
  * @author Sen Schwarz
  */
-public class VierGewinnt {
+public class VierGewinnt{
 
     private char spielfeld[][];
     
-    public VierGewinnt(){
-    }
+        public VierGewinnt(){
+        
+        }
    
-    public static void Spielstart ()
-    {
+        public static void Spielstart(){
         Spielreset();
-    }
+        }
     
-    public static void Spielreset()
-    {
+        public static void Spielreset(){
         
-    }
+        }
     
-    public static void Spielereingabe()
-    {
+        public static void Spielereingabe(){
         
-    }
-    
-    public static void spielfeldleer(char spielfeld[][]) 
-    {
-		for (char i = 0; i < spielfeld.length; i++) // y = Zeilen
-		{
-			for (char j = 0; j < spielfeld[i].length; j++) // x = Spalten
-			{
-				spielfeld[i][j] = ('y');												
-			}
-		}
-    } 
+        }
+     
     
     
     
     
-        public static void main(String[] args) {
-
-    }   
+        public static void main(String[] args){
+        new VierGewinnt();    
+        }
 }
+        
