ebe215cab7f62f688bfdbea08c35b8711fb64097
Sharingan1991 <senschwarz@yahoo.de>
2018-04-05 01:45:27 +0200

    Initial commit.

--- src/vier/gewinnt/VierGewinnt.java
+++ src/vier/gewinnt/VierGewinnt.java
@@ -6,24 +6,39 @@ package vier.gewinnt;
 public class VierGewinnt{
 
     private final char[][] spielfeld = new char[7][6];
-    private boolean spieler = true;
     private final Inputlistener listener;
+    
+    private boolean spieler = true; // Spieler1 = True, Spieler2= False
     
         public VierGewinnt(){
             listener = new Inputlistener();
         }
    
         public void Spielstart(){
-            Spielreset();
+            Spielreset(this.spielfeld[][]);
         }
     
-        public void Spielreset(){
+        public char[][] Spielreset(char spielfeld[][]){
         
+            for (char j = 0; j < spielfeld.length; j++) // j = Zeilen
+		{
+			for (char i = 0; i < spielfeld[j].length; i++) // i = Spalten
+			{
+				spielfeld[j][i] = ('9');
+                        }
+                }
+            return spielfeld;
         }
     
         public void Spielereingabe(){
         
         }
+        
+        
+
+		
+				
+	
         
         public void toggelSpielzug(){
             this.spieler = !this.spieler;   
@@ -38,7 +53,7 @@ public class VierGewinnt{
     
     
         public static void main(String[] args){
-            new VierGewinnt();    
+            new VierGewinnt();          
         }
 }
         
