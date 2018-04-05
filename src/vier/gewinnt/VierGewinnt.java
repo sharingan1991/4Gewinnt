c780c7e8ee31f4e604a0d2d75de0658964f89d3d
Sharingan1991 <senschwarz@yahoo.de>
2018-04-05 12:21:26 +0200

    Initial commit.

--- src/vier/gewinnt/VierGewinnt.java
+++ src/vier/gewinnt/VierGewinnt.java
@@ -5,7 +5,7 @@ package vier.gewinnt;
  */
 public class VierGewinnt{
 
-    private final int [][] spielfeld = new int[7][6]; //Spalten - Zeilen
+    private final int [][] spielfeld = new int[6][7]; //Zeilen - Spalten
     private final Inputlistener listener;
     
     private boolean spieler = true; // Spieler1 = True, Spieler2= False
@@ -106,7 +106,7 @@ public class VierGewinnt{
         }
         
         public static void main(String[] args){
-         
+        
         }
 }
         
